3d50b8eda3ec82afc46d6fadfc39441a8a93a772
Thomas Beaujean <thomas@appventus.com>
2014-05-07 11:04:45 +0200

    refactor cms_bundle to core_bundle

--- Entity/WidgetButton.php
+++ Entity/WidgetButton.php
@@ -2,7 +2,7 @@
 namespace Victoire\Widget\ButtonBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Victoire\CmsBundle\Entity\Widget;
+use Victoire\Bundle\CoreBundle\Entity\Widget;
 
 /**
  * WidgetButton
@@ -12,7 +12,7 @@ use Victoire\CmsBundle\Entity\Widget;
  */
 class WidgetButton extends Widget
 {
-    use \Victoire\CmsBundle\Entity\Traits\WidgetTrait;
+    use \Victoire\Bundle\CoreBundle\Entity\Traits\WidgetTrait;
 
     /**
      * @var string
--- Form/WidgetButtonType.php
+++ Form/WidgetButtonType.php
@@ -5,8 +5,8 @@ namespace Victoire\Widget\ButtonBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Victoire\CmsBundle\Form\EntityProxyFormType;
-use Victoire\CmsBundle\Form\WidgetType;
+use Victoire\Bundle\CoreBundle\Form\EntityProxyFormType;
+use Victoire\Bundle\CoreBundle\Form\WidgetType;
 
 
 /**
@@ -86,6 +86,6 @@ class WidgetButtonType extends WidgetType
      */
     public function getName()
     {
-        return 'appventus_victoirecmsbundle_widgetbuttontype';
+        return 'appventus_victoireCoreBundle_widgetbuttontype';
     }
 }
